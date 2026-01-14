eaec4d0db06b828cebe2ed1ee7ff797626adc588
Andrew Nesbitt <andrewnez@gmail.com>
2026-01-14 15:53:19 +0000

    Update URL and SHA256 for version 0.8.0

--- Formula/git-pkgs.rb
+++ Formula/git-pkgs.rb
@@ -1,8 +1,8 @@
 class GitPkgs < Formula
   desc "Track package dependencies across git history"
   homepage "https://github.com/andrew/git-pkgs"
-  url "https://github.com/andrew/git-pkgs/archive/refs/tags/v0.7.0.tar.gz"
-  sha256 "5c5aebf75e9570945b324777e5fa33cd5e35d31f6172c2415a4bd91db02477cc"
+  url "https://github.com/andrew/git-pkgs/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "b2e8ebfefc86fd137fb76225934c0fe2a1e01b2fcaac88a91f1134eecc4e9e71"
   license "AGPL-3.0"
 
   depends_on "cmake" => :build
